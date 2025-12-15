c41626a427c25ab28523e272f4a1901288402c16
thomasbroussard <thomas.broussard@gmail.com>
2025-12-15 09:54:25 +0100

    logging framework

--- calculator/src/test/java/fr/epita/calculator/tests/TestCalculator.java
+++ calculator/src/test/java/fr/epita/calculator/tests/TestCalculator.java
@@ -2,12 +2,24 @@ package fr.epita.calculator.tests;
 
 
 import fr.epita.calculator.services.Calculator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestCalculator {
+
+    private static final Logger LOGGER = LogManager.getLogger(TestCalculator.class);
+
+
+    @BeforeEach
+    public void setUp() {
+        LOGGER.info("setUp");
+    }
 
     @Test
     void shouldDivideTwoNumbers() {
@@ -21,5 +33,10 @@ public class TestCalculator {
         Calculator calc = new Calculator();
         assertThrows(ArithmeticException.class,
                 () -> calc.divide(10, 0));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        LOGGER.info("tearDown");
     }
 }
